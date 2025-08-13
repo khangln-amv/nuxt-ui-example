8b8c86f4a38077dbd6ce39553f79627ad031838d
khangln-amv <nguyenkhang-le@am-bition.vn>
2025-08-13 14:06:18 +0700

    update lang switcher

--- app/components/LanguageSwitcher.vue
+++ app/components/LanguageSwitcher.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 const { locale, locales, setLocale } = useI18n()
 
-const localeCodes = computed(() => locales.value.map((i) => i.code))
+const localeOptions = computed(() => locales.value.map((i) => ({
+  label: i.name,
+  value: i.code,
+})))
 
 const currentLocale = computed({
   get: () => locale.value,
@@ -10,5 +13,5 @@ const currentLocale = computed({
 </script>
 
 <template>
-  <USelect v-model="currentLocale" :items="localeCodes" class="w-48" />
+  <USelect v-model="currentLocale" :items="localeOptions" class="w-48" />
 </template>
